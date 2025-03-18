4d7851f41a8121e2f9fcc9b316266ae8cf772a90
Carlos Henrique <c.henrique.silva1103@outlook.com>
2025-03-18 14:31:01 -0300

    Atividade de história
    Atividade referente ao dia 18/03

---
+++ história.py
@@ -0,0 +1,6 @@
+name_protagonista = input("Qual o nome do(a) personagem? ")
+local = input("Onde será a história? ")
+arma = input("Escolha um arma para o combate: ")
+
+
+print(f"{name_protagonista} desafiou Elon Musk para um x1 dos crias bate panela. Na {local}, cercados por uma multidão vibrante, começaram o duelo. {name_protagonista} mostrou seu ritmo impecável, enquanto Elon usou uma {arma} construida para criar raios de lesões extraídos do jogador Neymar Jr. No final, as panelas soltaram faíscas e Elon admitiu: “Você ganhou. Mas da próxima vez, levarei uma baubau!”")
