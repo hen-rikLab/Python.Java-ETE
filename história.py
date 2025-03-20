e1de372937c458d90a57bd7f88e88d11f8cba0aa
CauaOliveira7737 <161508389+CauaOliveira7737@users.noreply.github.com>
2025-03-20 15:35:24 -0300

    Update história.py

--- história.py
+++ história.py
@@ -7,3 +7,11 @@ print(f"{name_protagonista} desafiou Elon Musk para um x1 dos crias bate panela.
       f"{name_protagonista} mostrou seu ritmo impecável, enquanto Elon usou uma {arma} "
       "construída para criar raios de lesões extraídos do jogador Neymar Jr. "
       "No final, as panelas soltaram faíscas e Elon admitiu: “Você ganhou. Mas da próxima vez, levarei uma baubau!”")
+print("DESEJA CONTINUAR A HISTÓRIA?")
+continuar = input("Digite S ou N: ")
+if continuar == "S":
+      print(f"Elon Musk: Você pensou que acabou? hahahahha. Eu treinei tomei o SUCOOO, agora estou mais forte, você não consegue me vencer mais. {name_protagonista}: Eu vou treinar!!!")
+      upgrade = input("Escolha seu upgrade: ")
+      print(f"Depois do upgrade({upgrade}), mais uma vez o Elon musk levou baubau!!")
+else:
+      print("Fim da História")
